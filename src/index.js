e270224a67954b2cfd6c1030b82b9d8b6c447c1e
tikenov <azamatkz90@gmail.com>
2019-03-13 18:26:51 +0600

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  s = [];
+  let m = new Map();
+  for (let b of bracketsConfig) m.set(b[0], b[1]);
+  for (let b of str) {
+    if (s[s.length - 1] == b) s.pop();
+    else if (m.has(b)) s.push(m.get(b));
+    else return false;
+  }
+  return !(s.length > 0);
+};
